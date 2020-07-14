77bbfc7c3710be29fe702745099190b470981594
Ramjee Ganti <code@ramjeeganti.com>
2020-07-14 22:41:14 +0530

    got the docker image with pytorch working

--- week1/handler.py
+++ week1/handler.py
@@ -1,11 +1,18 @@
 import json
-
+import torch
+from PIL import Image
+from torchvision import transforms
+import base64
+from requests_toolbelt.multipart import decoder
+import logging
+logger = logging.getLogger("Lambda Handler")
 
 def hello(event, context):
     body = {
         "message": "Go Serverless v1.0! Your function executed successfully!",
         "input": event,
     }
+    print('Testing hello of eva4p2')
 
     response = {"statusCode": 200, "body": json.dumps(body)}
 
@@ -19,3 +26,15 @@ def hello(event, context):
         "event": event
     }
     """
+def mobile_net_v2(event, context):
+    try:
+        content_type_header =event["headers"]["content-type"]
+        body = base64.b64decode(event["body"])
+        logger.info("Body Loaded")
+
+        picture = decoder.MultipartDecoder(body, content_type_header).parts[0]
+        picture_tensor = _transform_image(picture.content)
+        prediction = _get_prediction(image_tensor = picture_tensor)
+
+    except expression as identifier:
+        pass
