8bc9ef25673d8254f6658989ff0e17bae76dce6f
Ramjee Ganti <code@ramjeeganti.com>
2020-08-13 14:04:09 +0530

    init week3 project

---
+++ week3/src/libs/logger.py
@@ -0,0 +1,6 @@
+import logging
+
+# Initialize you log configuration using the base class
+logging.basicConfig(level=logging.INFO)
+# Retrieve the logger instance
+logger = logging.getLogger(__name__)
---
+++ week3/src/libs/utils.py
@@ -0,0 +1,40 @@
+try:
+    import unzip_requirements  # noqa
+except ImportError:
+    pass
+
+import base64
+from requests_toolbelt.multipart import decoder, encoder
+
+from src.logger import logger
+
+
+def get_image_from_event(event):
+    content_type_header = event["headers"]["content-type"]
+    body = base64.b64decode(event["body"])
+    logger.info("Body Loaded")
+    picture = decoder.MultipartDecoder(body, content_type_header).parts[0]
+    filename = get_picture_filename(picture).replace('"', "")
+    return picture, filename
+
+
+def get_picture_filename(picture):
+    try:
+        filename = picture.headers[b"Content-Disposition"].decode().split(":")[1].split("-")[1]
+        if 4 > len(filename):
+            filename = picture.headers[b"Content-Disposition"].decode().split(":")[2].split("-")[1]
+    except Exception as e:
+        filename = "not-found"
+        logger.exception(e)
+
+    return filename
+
+
+def get_multipartdata(file_path):
+
+    multipartdata = encoder.MultipartEncoder(fields={"file": (file_path, open(file_path, "rb"))})
+    return multipartdata
+
+
+def convert_multipartdata_base64(multipartdata):
+    return base64.b64encode(multipartdata.read()).decode("utf-8")
