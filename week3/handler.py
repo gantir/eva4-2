b69b5e9801763549357a0e9587d97b14b1219ad8
Ramjee Ganti <code@ramjeeganti.com>
2020-08-16 13:47:59 +0530

    synched with develop phase2

--- week3/handler.py
+++ week3/handler.py
@@ -4,9 +4,12 @@ except ImportError:
     pass
 
 import base64
+import cv2
 import json
+import numpy as np
 from src.libs import utils
 from src.libs.logger import logger
+from src.models.facerec.facerec import FaceRecognition
 
 
 headers = {
@@ -38,27 +41,57 @@ def hello(event, context):
     """
 
 
-def align_face(event, context):
+def face_align(event, context):
     try:
-        pictures = utils.get_image_from_event(event)
-        # picture_tensor = img_net.transform_image(pictures[0].content)
+        picture, picturename = utils.get_images_from_event(event, max_files=1)[0]
+        picture_ndarray = cv2.imdecode(np.frombuffer(picture.content, np.uint8), -1)
 
-        # has_human = check_human_image(picture_tensor) & check_face(picture_tensor)
-        has_human = False
-        if not has_human:
-            return {
-                "statusCode": 422,
-                "headers": headers,
-                "body": json.dumps({"error": "The submitted image doesn't have a human"}),
-            }
-        else:
-            # aligned_face = get_aligned_face(picture_tensor)
-            return {
-                "statusCode": 200,
-                "headers": headers,
-            }
+        f = FaceRecognition()
+        aligned_face = f.alignFace(picture_ndarray)
 
+        fields = {"file0": ("file0", base64.b64encode(aligned_face).decode("utf-8"), "image/jpg",)}
+
+        return {"statusCode": 200, "headers": headers, "body": json.dumps(fields)}
+
+    except ValueError as ve:
+        logger.exception(ve)
+        return {
+            "statusCode": 422,
+            "headers": headers,
+            "body": json.dumps({"error": repr(ve)}),
+        }
     except Exception as e:
+        logger.exception(e)
+        return {
+            "statusCode": 500,
+            "headers": headers,
+            "body": json.dumps({"error": repr(e)}),
+        }
+
+
+def face_mask(event, context):
+    try:
+        picture, picturename = utils.get_images_from_event(event, max_files=1)[0]
+
+        n95_msk_src_img = cv2.imread("data/3M-KN95-9501-Dust-Mask_v1.jpg")
+        picture_ndarray = cv2.imdecode(np.frombuffer(picture.content, np.uint8), -1)
+
+        f = FaceRecognition()
+        n95_msk_img = f.faceMask(n95_msk_src_img, picture_ndarray)
+
+        fields = {"file0": ("file0", base64.b64encode(n95_msk_img).decode("utf-8"), "image/jpg",)}
+
+        return {"statusCode": 200, "headers": headers, "body": json.dumps(fields)}
+
+    except ValueError as ve:
+        logger.exception(ve)
+        return {
+            "statusCode": 422,
+            "headers": headers,
+            "body": json.dumps({"error": repr(ve)}),
+        }
+    except Exception as e:
+        logger.exception(e)
         return {
             "statusCode": 500,
             "headers": headers,
@@ -69,20 +102,28 @@ def align_face(event, context):
 def face_swap(event, context):
     try:
         files = utils.get_images_from_event(event, max_files=2)
-
-        fields = {}
         if len(files) == 2:
-            for i, (file, _filename) in enumerate(files):
-                fields["file" + str(i)] = (
-                    "file" + str(i) + ".jpg",
-                    base64.b64encode(file.content).decode("utf-8"),
-                    "image/jpg",
-                )
+            src_img_ndarray = cv2.imdecode(np.frombuffer(files[0][0].content, np.uint8), -1)
+            dest_img_ndarray = cv2.imdecode(np.frombuffer(files[1][0].content, np.uint8), -1)
+            f = FaceRecognition()
+            swapped_img = f.faceSwap(src_img_ndarray, dest_img_ndarray)
+            fields = {"file0": ("file0", base64.b64encode(swapped_img).decode("utf-8"), "image/jpg",)}
 
             return {"statusCode": 200, "headers": headers, "body": json.dumps(fields)}
         else:
-            return {"statusCode": 400, "headers": headers, "body": "2 files couldn't be found in input"}
+            return {
+                "statusCode": 400,
+                "headers": headers,
+                "body": "Please pass exactly 2 files as input",
+            }
 
+    except ValueError as ve:
+        logger.exception(ve)
+        return {
+            "statusCode": 422,
+            "headers": headers,
+            "body": json.dumps({"error": repr(ve)}),
+        }
     except Exception as e:
         logger.exception(e)
         return {
